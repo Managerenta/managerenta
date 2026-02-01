88cbfbd68ec3759051ad88cee74f5f36e070ba32
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-01 10:54:58 +0100

    Refactor Navbar component to accept props for navHeight and background, and wrap with memo for performance optimization

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,5 +1,18 @@
 "use client";
+import { memo } from "react";
+import { NavbarStyled } from "./styled";
 
-export default function Navbar() {
-	return <div>Navbar</div>;
+interface IProps {
+	navHeight: string;
+	background: string;
 }
+
+function Navbar({ background, navHeight }: IProps) {
+	return (
+		<NavbarStyled $navHeight={navHeight} $background={background}>
+			Navbar
+		</NavbarStyled>
+	);
+}
+
+export default memo(Navbar);
