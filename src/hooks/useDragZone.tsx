50ad4cad1a9ed01fac93f0cffdc438acaf9f4c4a
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-07 13:06:07 +0100

    Refactor useDragZone hook to improve readability and maintainability

--- src/hooks/useDragZone.tsx
+++ src/hooks/useDragZone.tsx
@@ -1,50 +1,44 @@
 "use client";
-import { IUploadFile } from "@/types";
 import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
 import { v4 as uuid } from "uuid";
+import type { IUploadFile } from "@/types";
 
 const useDragZone = () => {
 	const [uploadedFiles, setUploadFiles] = useState<IUploadFile[]>([]);
 
-	const clearPreviousFiles = useCallback(
-		() => setUploadFiles?.([]),
-		[setUploadFiles],
-	);
+	const clearPreviousFiles = useCallback(() => setUploadFiles?.([]), []);
 
-	const handleFileUpload = useCallback(
-		(files: Blob[]): void => {
-			for (const file of files) {
-				const reader = new FileReader();
+	const handleFileUpload = useCallback((files: Blob[]): void => {
+		for (const file of files) {
+			const reader = new FileReader();
 
-				reader.onabort = () => {
-					toast.warn("file upload aborted", {
-						className: "file-upload-aborted",
-					});
+			reader.onabort = () => {
+				toast.warn("file upload aborted", {
+					className: "file-upload-aborted",
+				});
+			};
+			reader.onerror = () => {
+				toast.error("file upload failed", {
+					className: "file-upload-error",
+				});
+			};
+			reader.onload = () => {
+				const buffer = reader.result as ArrayBuffer;
+
+				const uploadedFile: IUploadFile = {
+					id: uuid(),
+					name: uuid(),
+					type: file.type,
+					buffer,
 				};
-				reader.onerror = () => {
-					toast.error("file upload failed", {
-						className: "file-upload-error",
-					});
-				};
-				reader.onload = () => {
-					const buffer = reader.result as ArrayBuffer;
 
-					const uploadedFile: IUploadFile = {
-						id: uuid(),
-						name: uuid(),
-						type: file.type,
-						buffer,
-					};
-
-					if (setUploadFiles)
-						setUploadFiles((files) => [...files, uploadedFile]);
-				};
-				reader.readAsArrayBuffer(file);
-			}
-		},
-		[setUploadFiles],
-	);
+				if (setUploadFiles)
+					setUploadFiles((files) => [...files, uploadedFile]);
+			};
+			reader.readAsArrayBuffer(file);
+		}
+	}, []);
 
 	return {
 		clearPreviousFiles,
