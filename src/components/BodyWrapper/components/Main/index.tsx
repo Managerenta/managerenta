018e464e3b338f2577acc5b5324ad37f83957c0f
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-07 13:04:32 +0100

    Refactor Main component to conditionally render Navbar based on excluded routes

--- src/components/BodyWrapper/components/Main/index.tsx
+++ src/components/BodyWrapper/components/Main/index.tsx
@@ -1,10 +1,11 @@
 "use client";
 import dynamic from "next/dynamic";
-import { memo, type ReactNode, useContext } from "react";
-import { BoxStyled as Box } from "@/components/Box/styled";
+import { usePathname } from "next/navigation";
+import { memo, type ReactNode, useContext, useMemo, useState } from "react";
+import Box from "@/components/Box";
 import { AppContextProvider } from "@/hooks";
-import { MainStyled } from "./styled";
 import { Navbar, Toast } from "@/layouts";
+import { MainStyled } from "./styled";
 
 export const Modal = dynamic(
 	() => import("@/layouts").then((mod) => mod.Modal),
@@ -18,14 +19,28 @@ interface IProps {
 }
 
 function Main({ children }: IProps) {
+	const [excludedRoutes] = useState(["/login", "/signup"]);
+
 	const { navHeight } = useContext(AppContextProvider);
+	const pathname = usePathname();
+
+	// const currentPath = typeof window !== "undefined" ? window.location.pathname : "";
+	const shouldExcludeNavbar = useMemo(
+		() => excludedRoutes.includes(pathname),
+		[pathname, excludedRoutes],
+	);
 
 	return (
 		<MainStyled $navHeight={navHeight}>
 			<Box className="body-wrapper">
 				<Toast />
 
-				<Navbar navHeight={navHeight} background="var(--Secondary-600)" />
+				{!shouldExcludeNavbar && (
+					<Navbar
+						navHeight={navHeight}
+						background="var(--Secondary-600)"
+					/>
+				)}
 				<Box className="body-container-max-width">{children}</Box>
 			</Box>
 		</MainStyled>
