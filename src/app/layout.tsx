f45ee684371d36cece38319fbceb14d31e0a848a
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-07 13:04:15 +0100

    Refactor layout.tsx to comment out unused font imports and clean up body className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
 import {
 	DM_Sans,
-	Iceland,
-	Rajdhani,
-	Unbounded,
-	Ysabeau_Office,
+	// Iceland,
+	// Rajdhani,
+	// Unbounded,
+	// Ysabeau_Office,
+	Roboto,
 } from "next/font/google";
 import { cookies } from "next/headers";
 import { BodyWrapper } from "@/components";
 import { defaultEnvOptions, verifyUserLogin } from "@/constants";
-
-const unbounded = Unbounded({
-	subsets: ["latin"],
-	weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
-});
 
 const dm_sans = DM_Sans({
 	subsets: ["latin"],
@@ -31,20 +27,30 @@ const dm_sans = DM_Sans({
 	],
 });
 
-const rajdhani = Rajdhani({
+const roboto = Roboto({
 	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
+	weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-const iceland = Iceland({
-	subsets: ["latin"],
-	weight: ["400"],
-});
+// const unbounded = Unbounded({
+// 	subsets: ["latin"],
+// 	weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+// });
 
-const ysabeau_office = Ysabeau_Office({
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700", "800", "900"],
-});
+// const rajdhani = Rajdhani({
+// 	subsets: ["latin"],
+// 	weight: ["300", "400", "500", "600", "700"],
+// });
+
+// const iceland = Iceland({
+// 	subsets: ["latin"],
+// 	weight: ["400"],
+// });
+
+// const ysabeau_office = Ysabeau_Office({
+// 	subsets: ["latin"],
+// 	weight: ["400", "500", "600", "700", "800", "900"],
+// });
 
 export default async function RootLayout({
 	children,
@@ -65,10 +71,12 @@ export default async function RootLayout({
 
 	return (
 		<html lang="en">
-			<body
-				className={`${unbounded.className} ${dm_sans.className} ${rajdhani.className} ${iceland.className} ${ysabeau_office.className}`}>
+			<body className={`${dm_sans.className} ${roboto.className}`}>
 				<div id="modal-popup"></div>
-				<BodyWrapper env={env} isUserSessionActive={isUserSessionActive}>
+				<BodyWrapper
+					env={env}
+					isUserSessionActive={isUserSessionActive}
+				>
 					{children}
 				</BodyWrapper>
 			</body>
