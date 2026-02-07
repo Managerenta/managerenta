f3b3655b5dd7222aca9d4e0ba21d268cf358a699
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-07 13:06:19 +0100

    Add useReviews hook to manage property reviews

---
+++ src/hooks/Reviews/useReviews.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useMemo } from "react";
+
+export default function useReviews() {
+	const reviews = useMemo(() => {
+		const lists: {
+			id: string;
+			name: string;
+			label: string;
+			photo?: string;
+			comment: string;
+		}[] = [
+			{
+				id: "1",
+				name: "Adebayo Okonkwo",
+				label: "Lagos Property Owner",
+				photo: "/images/landlords/landlord-1.png",
+				comment:
+					"PropertyTrack helped me organize my 15 properties and increased my rent collection by 40%",
+			},
+		];
+
+		return lists;
+	}, []);
+
+	return { reviews };
+}
