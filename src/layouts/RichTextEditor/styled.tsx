c6994ab08bf63fedd75aa1c31b60a8d5e06bb71b
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-07 13:07:43 +0100

    Remove RichTextEditor styled component and its associated styles

--- src/layouts/RichTextEditor/styled.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import styled from "styled-components";
-import { Box } from "../../components";
-
-export const RichTextEditorStyled = styled(Box)`
-	width: 100%;
-	height: 100%;
-`;
