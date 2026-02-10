6a17248d07d560c9e18dc9cbf8a88a8891b3258b
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-10 12:51:29 +0100

    Add styled Header component for layout and typography

---
+++ src/layouts/UserAuthWrapper/components/Header/styled.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+	display: flex;
+	flex-direction: column;
+	gap: 5px;
+	align-items: center;
+	justify-content: center;
+
+	h1 {
+		font-size: 24px;
+		font-weight: 700;
+	}
+
+	p {
+		font-size: 16px;
+		color: var(--Secondary-100);
+	}
+`;
