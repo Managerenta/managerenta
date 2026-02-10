29e6c09b8e8d2c7758035f110a62da643003ef3b
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-10 10:23:03 +0100

    Add KeyHighlights component to showcase property management features

---
+++ src/layouts/UserAuthWrapper/components/KeyHighlights/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { memo, useMemo } from "react";
+import { BsCalculator } from "react-icons/bs";
+import { LiaCheckCircle } from "react-icons/lia";
+import { Text } from "@/components";
+import Reviews from "./components/Reviews";
+import { KeyHighlightsStyled } from "./styled";
+
+function KeyHighlights() {
+	const lists = useMemo(() => {
+		const data: string[] = [
+			"Never miss a rent payment",
+			"Track all properties in one place",
+			"Automated tenant reminders",
+			"Complete transaction history",
+			"Generate detailed reports",
+			"Collaborate with team members",
+		];
+		return data.map((item, index) => {
+			return (
+				<li key={index}>
+					<LiaCheckCircle />
+					<Text>{item}</Text>
+				</li>
+			);
+		});
+	}, []);
+
+	return (
+		<KeyHighlightsStyled>
+			<header>
+				<BsCalculator />
+				<Text>PropertyTrack</Text>
+			</header>
+
+			<Text className="title">Start Managing Smarter Today</Text>
+
+			<ul>{lists}</ul>
+
+			<Reviews />
+		</KeyHighlightsStyled>
+	);
+}
+
+export default memo(KeyHighlights);
