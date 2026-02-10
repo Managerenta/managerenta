fe75034fcfdf2643205bf6df0fc59c4439da4d7b
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-02-10 10:23:28 +0100

    Add Reviews component to display user feedback in KeyHighlights

---
+++ src/layouts/UserAuthWrapper/components/KeyHighlights/components/Reviews/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useMemo } from "react";
+import { Box, Image, Text } from "@/components";
+import { useReviews } from "@/hooks";
+import { ReviewsStyled } from "./styled";
+
+export default function Reviews() {
+	const { reviews } = useReviews();
+
+	const reviewsList = useMemo(() => {
+		return reviews.map((review) => {
+			return (
+				<Box key={review.id} className="review">
+					<Box className="header">
+						<Image url={review.photo || ""} alt={review.name} />
+						<Box>
+							<h4>{review.name}</h4>
+							<Text>{review.label}</Text>
+						</Box>
+					</Box>
+					<Text>"{review.comment}"</Text>
+				</Box>
+			);
+		});
+	}, [reviews]);
+
+	return <ReviewsStyled>{reviewsList}</ReviewsStyled>;
+}
