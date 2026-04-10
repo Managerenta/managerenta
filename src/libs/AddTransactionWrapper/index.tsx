d7c7300aebe5e02cdce4c2eebbe8a54f569e4385
Abdulwaarith <abdulwaarithz@gmail.com>
2026-04-10 19:36:56 +0100

    wire AddTransactionWrapper to real tenant data via tenantId prop

--- src/libs/AddTransactionWrapper/index.tsx
+++ src/libs/AddTransactionWrapper/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { memo } from "react";
 import { Box } from "@/components";
+import { useTenantDetail } from "@/hooks";
 import {
 	AddTransactionHeader,
 	KeyboardShortcutsPanel,
@@ -11,19 +12,45 @@ import {
 } from "./components";
 import { AddTransactionWrapperStyled } from "./styled";
 
-function AddTransactionWrapper() {
+interface IProps {
+	tenantId: string;
+}
+
+function AddTransactionWrapper({ tenantId }: IProps) {
+	const { tenantDetail, transactions, paymentStats, isLoading } =
+		useTenantDetail(tenantId);
+
 	return (
 		<AddTransactionWrapperStyled>
-			<AddTransactionHeader />
+			<AddTransactionHeader
+				tenantName={tenantDetail?.name ?? ""}
+				tenantId={tenantId}
+			/>
 
 			<Box className="content-grid">
 				<Box className="left-column">
-					<TenantSummary />
-					<TransactionForm />
+					<TenantSummary
+						name={tenantDetail?.name ?? ""}
+						avatar={tenantDetail?.avatar ?? ""}
+						property={tenantDetail?.property ?? ""}
+						unit={tenantDetail?.unit ?? ""}
+						outstandingBalance={paymentStats.outstandingBalance}
+						isOverdue={(tenantDetail?.overdueDays ?? 0) > 0}
+						isLoading={isLoading}
+					/>
+					<TransactionForm
+						tenantId={tenantId}
+						monthlyRent={tenantDetail?.monthlyRent ?? ""}
+					/>
 				</Box>
 				<Box className="right-column">
-					<PaymentHistoryPanel />
-					<SmartSuggestionsPanel />
+					<PaymentHistoryPanel
+						transactions={transactions.slice(0, 3)}
+						isLoading={isLoading}
+					/>
+					<SmartSuggestionsPanel
+						monthlyRent={tenantDetail?.monthlyRent ?? ""}
+					/>
 					<KeyboardShortcutsPanel />
 				</Box>
 			</Box>
