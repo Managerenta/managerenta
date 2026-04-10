d95a9b0b84d801728d2aa876ea72a475ae514e7a
Abdulwaarith <abdulwaarithz@gmail.com>
2026-04-10 19:35:55 +0100

    use Secondary-500 for separator color

--- src/layouts/AlternativeSeparator/styled.tsx
+++ src/layouts/AlternativeSeparator/styled.tsx
@@ -12,12 +12,12 @@ export const AlternativeSeparatorStyled = styled(Box)`
 	hr {
 		flex: 1;
 		border: none;
-		border-top: 1px solid var(--Secondary-100);
+		border-top: 1px solid var(--Secondary-500);
 	}
 
 	p {
 		font-size: 14px;
-		color: var(--Secondary-100);
+		color: var(--Secondary-500);
 		font-weight: 400;
 	}
 `;
