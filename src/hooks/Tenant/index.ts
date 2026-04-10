accb6eceaa9234b93ec5b761da06d12f47b4b707
Abdulwaarith <abdulwaarithz@gmail.com>
2026-04-10 19:34:46 +0100

    rename useTenants export to useTenantsData

--- src/hooks/Tenant/index.ts
+++ src/hooks/Tenant/index.ts
@@ -1,3 +1,3 @@
 export { default as useTenantDetail } from "./useTenantDetail";
 export { default as useTenantNavigation } from "./useTenantNavigations";
-export { default as useTenants } from "./useTenants";
+export { default as useTenantsData } from "./useTenants";
