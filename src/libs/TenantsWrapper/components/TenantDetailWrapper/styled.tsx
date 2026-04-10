fa6c2db43dd0cb99eea0b211fe766ead69f3ae23
Abdulwaarith <abdulwaarithz@gmail.com>
2026-04-10 19:46:57 +0100

    tighten tenant detail wrapper padding

--- src/libs/TenantsWrapper/components/TenantDetailWrapper/styled.tsx
+++ src/libs/TenantsWrapper/components/TenantDetailWrapper/styled.tsx
@@ -7,7 +7,7 @@ export const TenantDetailWrapperStyled = styled(Box)`
 	flex-direction: column;
 	gap: 20px;
 	width: 100%;
-	padding: 20px;
+	padding: 10px 5px;
 	background: #f8fafc;
 
 	.bottom-grid {
