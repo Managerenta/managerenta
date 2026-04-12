effb68571bf624ca182942ba4928a49db4f19873
Abdulwaarith <abdulwaarithz@gmail.com>
2026-04-12 08:38:46 +0100

    use axios baseURL for auth endpoints

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,10 +1,7 @@
 import api from "./api";
-import defaultEnvOptions from "./defaultEnvOptions";
-
-const getBaseUrl = () => defaultEnvOptions().MAIN_SERVICE_URL;
 
 export async function loginUser(email: string, password: string) {
-	return await api().post(`${getBaseUrl()}/api/auth/login`, {
+	return await api().post("/api/auth/login", {
 		email,
 		password,
 	});
@@ -23,7 +20,7 @@ export async function signupUser(payload: {
 	formData.append("email", payload.email);
 	formData.append("password", payload.createPassword);
 
-	return await api().post(`${getBaseUrl()}/api/auth/signup`, formData);
+	return await api().post("/api/auth/signup", formData);
 }
 
 export async function logoutUser() {
