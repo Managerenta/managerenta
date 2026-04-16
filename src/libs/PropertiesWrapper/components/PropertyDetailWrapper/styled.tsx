239e4f38b05840573a1a4f539baeadc374f6e055
Abdulwaarith <abdulwaarithz@gmail.com>
2026-04-16 13:01:21 +0100

    dark mode token fix in PropertyDetailWrapper

--- src/libs/PropertiesWrapper/components/PropertyDetailWrapper/styled.tsx
+++ src/libs/PropertiesWrapper/components/PropertyDetailWrapper/styled.tsx
@@ -8,7 +8,7 @@ export const PropertyDetailWrapperStyled = styled(Box)`
 	gap: 20px;
 	width: 100%;
 	padding: 10px 5px;
-	background: #f8fafc;
+	background: var(--Surface-Page);
 
 	.bottom-grid {
 		display: grid;
