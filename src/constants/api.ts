f979b68690e3485fdb3dd6ded4d2a578e7d60900
Abdulwaarith <abdulwaarithz@gmail.com>
2026-05-11 08:36:35 +0100

    fix: use undefined instead of empty string for missing baseURL

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosRequestConfig } from "axios";
 
 const axiosClient = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_MAIN_SERVICE_URL || "",
+	baseURL: process.env.NEXT_PUBLIC_MAIN_SERVICE_URL || undefined,
 	withCredentials: true,
 });
 
