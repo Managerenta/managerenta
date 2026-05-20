1fc27142fa74477a5466c75869a784bec4d2ea36
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-05-20 20:27:29 +0100

    feat: remove standalone output configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	allowedDevOrigins: ["192.168.18.10"],
-	output: "standalone",
 	reactStrictMode: true,
 	poweredByHeader: false,
 	compiler: {
