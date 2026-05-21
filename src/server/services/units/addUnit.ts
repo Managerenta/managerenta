afb65b3034aedc705f7e7833b5849bc374a2bb62
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-05-21 14:38:07 +0100

    refactor(units): streamline addUnit function and improve error handling

--- src/server/services/units/addUnit.ts
+++ src/server/services/units/addUnit.ts
@@ -1,5 +1,9 @@
-import mongoose from "mongoose";
-import { createUnitDB, incrementPropertyTotalUnitsDB } from "../../models";
+import { ErrPropertyNotFound } from "../../constants";
+import {
+	createUnitDB,
+	getPropertyByIdDB,
+	incrementPropertyTotalUnitsDB,
+} from "../../models";
 import { invalidateCacheKeys } from "./utils";
 
 export default async function addUnit({
@@ -13,31 +17,15 @@ export default async function addUnit({
 	propertyId: string;
 	userId: string;
 }) {
-	const session = await mongoose.startSession();
-	try {
-		session.startTransaction();
-		const unit = await createUnitDB({
-			payload: { name, rent, propertyId, userId },
-			session,
-		});
-		if (!unit) {
-			await session.abortTransaction();
-			return null;
-		}
+	const property = await getPropertyByIdDB({ id: propertyId, userId });
+	if (!property) throw ErrPropertyNotFound;
 
-		await incrementPropertyTotalUnitsDB({
-			id: propertyId,
-			userId,
-			session,
-		});
+	const unit = await createUnitDB({
+		payload: { name, rent, propertyId, userId },
+	});
+	if (!unit) return null;
 
-		await session.commitTransaction();
-		await invalidateCacheKeys({ userId, propertyId });
-		return unit;
-	} catch (err) {
-		if (session.inTransaction()) await session.abortTransaction();
-		throw err;
-	} finally {
-		await session.endSession();
-	}
+	await incrementPropertyTotalUnitsDB({ id: propertyId, userId });
+	await invalidateCacheKeys({ userId, propertyId });
+	return unit;
 }
